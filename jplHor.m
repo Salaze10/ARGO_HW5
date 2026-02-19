a225a3b58605ce6ffaf5d085af6150022d461a6a
Sanchc40 <sanchc40@my.erau.edu>
2026-02-19 10:52:43 -0700

    Update jplHor.m

--- jplHor.m
+++ jplHor.m
@@ -14,8 +14,8 @@ clc
 %-----------------------
 % Depature Window (Gregorian dates)
 start1 = datetime(2042, 01, 01);     % start of depature window
-end1   = datetime(2042, 02, 01);     % end of depature window
-gd1 = (start1 : days(1) : end1)';    % generates array with timesteps of 1 day
+end1   = datetime(2042, 01, 03);     % end of depature window
+gd1 = (start1 : minutes(1) : end1)';    % generates array with timesteps of 1 minute
 jd1 = juliandate(gd1);               % Convert Gregian dates to Julian dates
 
 % ----------------------
@@ -37,8 +37,8 @@ inc = 0;                               % deg
 alt = 20000;                           % km
 
 % Orbit Geometry & Initial State
-r_sc_p = R_mars + alt;                         % Radius at Periapsis (km)
-a      = r_sc_p / (1 - ecc);                   % Semi-Major Axis of S/C Orbit (km)
+r_sc_p = R_mars + alt;                         % Radius at Apoapsis (km)
+a      = r_sc_p / (1 + ecc);                   % Semi-Major Axis of S/C Orbit (km)
 v_p    = sqrt(mu_mars * ((2/r_sc_p) - (1/a))); % Vis-Viva Velocity at Periapsis (km/s)
 n      = sqrt(mu_mars / a^3);                  % Mean Motion (rad/s)
 T_period = 2*pi*sqrt(a^3/mu_mars);             % Orbital Period (s)
@@ -140,6 +140,117 @@ xlabel("% Sunlight")
 ylabel("time [days]")
 grid on
 
+[S_percent, gd1] = Scalc(e,alt,R_mars,R_sun,mu_mars);
+
+% Plot
+figure(5);
+plot(gd1, S_percent,'LineWidth',1.2);
+xlabel('Time');
+ylabel('S (%)');
+ylim([0 100]);
+grid on
+
+function [S_percent, gd] = Scalc(e,alt,R_mars,R_sun,mu_mars)
+    r_a = R_mars + alt;             % apoapsis radius (km)
+    a  = r_a/(1+e);
+    
+    % Time Setup
+    start = datetime(2042,01,01,0,0,0);
+    END = datetime(2042,01,03,0,0,0);
+    gd = (start:minutes(1):END)';
+    N = length(gd);
+
+    % Get Mars heliocentric position
+    [r_mars_vec, ~] = get_jpl_horizons('499', start, END);
+
+    % Safety check
+    if length(r_mars_vec) ~= N
+        error('Horizons time vector does not match gd1 length.')
+    end
+
+    % Preallocate
+    S_percent = zeros(N,1);
+
+    % Mean motion
+    n = sqrt(mu_mars/a^3);
+
+    for k = 1:N
+    
+        t = (k-1)*60;    % 60 seconds per step (1 minute resolution)
+    
+        % Mean anomaly
+        M = n*t;
+    
+        % Solve Kepler's Equation (Newton iteration)
+        E = M;
+        for iter = 1:20
+            E = E - (E - e*sin(E) - M)/(1 - e*cos(E));
+        end
+    
+        % True anomaly
+        nu = 2*atan2(sqrt(1+e)*sin(E/2), sqrt(1-e)*cos(E/2));
+    
+        % Radius
+        r = a*(1 - e*cos(E));
+    
+        % Spacecraft Mars-centered position
+        r_sc_mars = [r*cos(nu), r*sin(nu), 0];
+    
+        % Spacecraft heliocentric position
+        r_sc_sun = r_mars_vec(k,:) + r_sc_mars;
+    
+        % Distances
+        d_sun  = norm(r_sc_sun);
+        d_mars = norm(r_sc_mars);
+    
+        % Angular radii
+        alpha_s = asin( min(1, max(-1, R_sun/d_sun)) );
+        alpha_m = asin( min(1, max(-1, R_mars/d_mars)) );
+    
+        % Angular separation
+        cos_theta = dot(-r_sc_sun, -r_sc_mars)/(d_sun*d_mars);
+        cos_theta = min(1, max(-1, cos_theta));
+        theta = acos(cos_theta);
+    
+        % Eclipse logic
+        if theta >= alpha_s + alpha_m
+            S_percent(k) = 100;
+        
+        elseif alpha_m >= alpha_s && theta <= (alpha_m - alpha_s)
+            S_percent(k) = 0;
+        
+        else
+            % Partial eclipse (safe overlap math)
+            r1 = alpha_s;
+            r2 = alpha_m;
+            d  = theta;
+        
+            % Prevent divide-by-zero
+            if d == 0
+                S_percent(k) = 0;
+            else
+                arg1 = (d^2 + r1^2 - r2^2)/(2*d*r1);
+                arg2 = (d^2 + r2^2 - r1^2)/(2*d*r2);
+            
+                arg1 = min(1,max(-1,arg1));
+                arg2 = min(1,max(-1,arg2));
+            
+                part1 = r1^2 * acos(arg1);
+                part2 = r2^2 * acos(arg2);
+                part3 = 0.5*sqrt(max(0,(-d+r1+r2)*(d+r1-r2)*(d-r1+r2)*(d+r1+r2)));
+            
+                overlap = part1 + part2 - part3;
+                S_percent(k) = 100*(1 - overlap/(pi*r1^2));
+            end
+        end
+    end
+    
+% Clean numerical noise
+S_percent = real(S_percent);
+S_percent(isnan(S_percent)) = 0;
+S_percent = max(0,min(100,S_percent));
+end
+
 % ----------------------
 % JPL HORIZONS API
 % ----------------------
@@ -164,7 +275,7 @@ function [r_vec, v_vec] = get_jpl_horizons(body_id, t_start, t_end)
             'CENTER', '@10', ...
             'START_TIME', s_str, ...
             'STOP_TIME', e_str, ...
-            'STEP_SIZE', '1d', ...
+            'STEP_SIZE', '1m', ...
             'CSV_FORMAT', 'YES');
             
         % Parse Text
@@ -188,5 +299,6 @@ function [r_vec, v_vec] = get_jpl_horizons(body_id, t_start, t_end)
         r_vec = [C{3}, C{4}, C{5}];
         v_vec = [C{6}, C{7}, C{8}];
 end
+
 
 
